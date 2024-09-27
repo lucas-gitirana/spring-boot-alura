8c20032f8063904e36f0ba20369243232ae2d6f0
Lucas E. Gitirana <gitiranalucas5@gmail.com>
2024-09-26 21:17:43 -0300

    finalizando classe principal com lambdas

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.principal.Principal;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
@@ -21,39 +22,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@java.lang.Override
 	public void run(java.lang.String... args) throws Exception {
-		var consumoApi = new ConsumoAPI();
-
-		//API do OMDB
-		//&apikey=d924e6d8
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&apikey=d924e6d8");
-		System.out.println(json);
-
-		//API DE CAFÉ
-		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		//System.out.println(json);
-
-		// Instanciando classe conversora
-		ConverteDados conversor = new ConverteDados();
-
-		// Obtendo dados da série
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&apikey=d924e6d8");
-		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dadosSerie);
-
-		// Obtendo dados do episódio
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&season=1&episode=1&apikey=d924e6d8");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		//Obtendo dados de temporadas
-		List<DadosTemporada> dadosTemporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&season=" +i+ "&apikey=d924e6d8");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			dadosTemporadas.add(dadosTemporada);
-		}
-
-		dadosTemporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,13 +1,19 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner s = new Scanner(System.in);
     private ConsumoAPI consumo = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
 
     private static final String ENDERECO = "https://www.omdbapi.com/?t=";
     private static final String API_KEY = "&apikey=d924e6d8";
@@ -16,5 +22,21 @@ public class Principal {
         System.out.println("Informe o nome da série: ");
         var nomeSerie = s.nextLine();
         var json = consumo.obterDados(ENDERECO +nomeSerie.replace(" ", "+")+ API_KEY);
+
+        // Carregando série
+        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+
+        //Carregando temporadas
+        List<DadosTemporada> dadosTemporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" +i+ API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            dadosTemporadas.add(dadosTemporada);
+        }
+        dadosTemporadas.forEach(System.out::println);
+
+        dadosTemporadas.forEach(t -> t.episodios().forEach( e -> System.out.println(e.titulo())));
     }
 }
